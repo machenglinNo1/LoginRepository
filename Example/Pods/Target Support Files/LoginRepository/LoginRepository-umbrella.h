0b50f6c5ae28185f7f0fc10dbfa6668a03cb9c8a
马成麟 <machenglin@5i5j.com>
2020-12-14 16:16:52 +0800

    提交登录组件

---
+++ Example/Pods/Target Support Files/LoginRepository/LoginRepository-umbrella.h
@@ -0,0 +1,27 @@
+#ifdef __OBJC__
+#import <UIKit/UIKit.h>
+#else
+#ifndef FOUNDATION_EXPORT
+#if defined(__cplusplus)
+#define FOUNDATION_EXPORT extern "C"
+#else
+#define FOUNDATION_EXPORT extern
+#endif
+#endif
+#endif
+
+#import "AESCipher.h"
+#import "AppSettings.h"
+#import "CLRequest.h"
+#import "CMReachableHelper.h"
+#import "CMUtils.h"
+#import "CWStarRateView.h"
+#import "JKCountDownButton.h"
+#import "LSDatePickerViewModel.h"
+#import "LSSkipAnimationHelper.h"
+#import "LSUpLoadHelper.h"
+#import "RegularExpressionsTool.h"
+
+FOUNDATION_EXPORT double LoginRepositoryVersionNumber;
+FOUNDATION_EXPORT const unsigned char LoginRepositoryVersionString[];
+
